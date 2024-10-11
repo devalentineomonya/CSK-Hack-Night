c0e78c4ebc81c458d639754a702c585de7952fa5
Valentine Omonya <valomosh254@gmail.com>
2024-10-11 20:14:48 +0300

    Changed soe files

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -52,13 +52,14 @@ const Hero = () => {
   }, []);
 
   return (
+
     <section className="h-fit min-h-screen w-full gradient-background -mt-16 flex items-center justify-center max-xl:px-4 max-xl:pt-32 max-xl:pb-12">
       <div className="max-w-[1400px] w-full min-h-48 flex max-lg:flex-col gap-x-20">
         <div className="max-w-2xl flex-1">
           <div className="font-medium text-lg xs:text-3xl lg:text-2xl xl:text-5xl text-white typing-effect">
-            <TypingEffect words={[`CyberEyes{W3lc0me_t0_`]} />
+            <TypingEffect words={[`Welcome to CSK`]} />
           </div>
-          <h3 className="text-xl xs:text-4xl lg:text-3xl xl:text-6xl py-4 text-white hero-title">{` Inaugural_h4ck_N1ght}`}</h3>
+          <h3 className="text-xl xs:text-4xl lg:text-3xl xl:text-6xl py-4 text-white hero-title">{`Hack Night`}</h3>
           <p className="font-light text-sm xs:text-lg text-white description">
             Join us for an exciting night of hacking, learning, and collaboration!
             Whether you&apos;re a beginner or an experienced cyber security enthusiast,
